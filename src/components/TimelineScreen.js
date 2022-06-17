e176e4a53f724b78779ff7f9cb28dc14c47c0835
Icaro-pavani <icaro.pavani79@gmail.com>
2022-06-17 03:30:43 -0300

    fix: layout posts

--- src/components/TimelineScreen.js
+++ src/components/TimelineScreen.js
@@ -7,95 +7,92 @@ import PublishPost from "./PublishPost.js";
 import PostCard from "./shared/PostCard.js";
 
 export default function TimelineScreen() {
+  // eslint-disable-next-line
+  const [refreshTimeline, setRefreshTimeline] = useState(false);
+  const [posts, setPosts] = useState(["initial"]);
 
+  const URL = "https://projeto17-linkr-cdio.herokuapp.com/";
+
+  useEffect(() => {
+    requestGetPosts();
     // eslint-disable-next-line
-    const [refreshTimeline, setRefreshTimeline] = useState(false);
-    const [posts, setPosts] = useState(["initial"]);
+  }, [refreshTimeline]);
 
+  async function requestGetPosts() {
+    try {
+      const response = await axios.get(`${URL}posts`);
+      setPosts(response.data);
+    } catch (e) {
+      setPosts(["error"]);
+      console.log(e);
+    }
+  }
 
-    useEffect(() => {
-        requestGetPosts();
-        // eslint-disable-next-line
-    }, [refreshTimeline]);
+  function renderPosts(posts) {
+    console.log("aqui");
 
-    async function requestGetPosts() {
-        try {
-            const response = await axios.get("http://localhost:4000/posts");
-            setPosts(response.data);
-            
-        } catch (e) {
-            setPosts(["error"]);
-            console.log(e);
-        }
+    if (posts.length === 0) {
+      return (
+        <div className="message-container">
+          <p className="message">There are no posts yet</p>
+        </div>
+      );
     }
 
-    function renderPosts(posts) {
-        console.log("aqui");
-
-        if (posts.length === 0) {
-            return (
-                <div className="message-container">
-                    <p className="message">There are no posts yet</p>
-                </div>
-            )
-        }
-
-        if (posts[0] === "error") {
-
-            return (
-                <div className="message-container">
-                    <p className="message">An error occured while trying to fetch the posts, please refresh the page</p>
-                </div>
-            )
-        }
-
-        return (
-            posts.map(post => {
-                const { id } = post;
-
-                return (
-                    <PostCard
-                        key={id}
-                        post={post}
-                    />
-                );
-            })
-        );
-
+    if (posts[0] === "error") {
+      return (
+        <div className="message-container">
+          <p className="message">
+            An error occured while trying to fetch the posts, please refresh the
+            page
+          </p>
+        </div>
+      );
     }
 
-    return posts[0] === "initial" ? (
-        <Div>
-            <HeaderBar />
-            <h1>timeline</h1>
-            <PublishPost refreshTimeline={refreshTimeline} setRefreshTimeline={setRefreshTimeline}/>
-            <div className="message-container">
-                <p className="message">Loading . . .</p>
-            </div>
-        </Div>
-    ) : (
-        <Div>
-            <HeaderBar />
-            <h1>timeline</h1>
-            <PublishPost refreshTimeline={refreshTimeline} setRefreshTimeline={setRefreshTimeline}/>
-            {
-                renderPosts(posts)
-            }
-        </Div>
-    );
+    return posts.map((post) => {
+      const { id } = post;
+
+      return <PostCard key={id} post={post} />;
+    });
+  }
+
+  return posts[0] === "initial" ? (
+    <Div>
+      <HeaderBar />
+      <h1>timeline</h1>
+      <PublishPost
+        refreshTimeline={refreshTimeline}
+        setRefreshTimeline={setRefreshTimeline}
+      />
+      <div className="message-container">
+        <p className="message">Loading . . .</p>
+      </div>
+    </Div>
+  ) : (
+    <Div>
+      <HeaderBar />
+      <h1>timeline</h1>
+      <PublishPost
+        refreshTimeline={refreshTimeline}
+        setRefreshTimeline={setRefreshTimeline}
+      />
+      {renderPosts(posts)}
+    </Div>
+  );
 }
 
 const Div = styled.div`
-h1 {
-    font-family: 'Oswald';
+  h1 {
+    font-family: "Oswald";
     font-weight: 700;
     font-size: 33px;
     line-height: 49px;
     color: white;
     margin: calc(19px + 72px) 0 19px 17px;
-}
+  }
 
-.message-container {
+  .message-container {
     height: auto;
     width: 80vw;
     display: flex;
@@ -105,17 +102,14 @@ h1 {
     color: white;
     margin: 0 auto;
     margin-top: 30px;
-    
-    
-}
+  }
 
-.message {
-    font-family: 'Lato';
+  .message {
+    font-family: "Lato";
     font-style: normal;
     font-weight: 400;
     font-size: 22px;
     line-height: 33px;
     text-align: center;
-}
-
-`
+  }
+`;
--- src/components/shared/PostCard.js
+++ src/components/shared/PostCard.js
@@ -1,212 +1,222 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import {IoHeartOutline,IoHeart} from "react-icons/io5";
+import { IoHeartOutline, IoHeart } from "react-icons/io5";
 import UserContext from "../../context/UserContext";
+import LoadingContext from "../../context/LoadingContext";
 
 export default function PostCard(props) {
-    
-    const { link, description, picture, username, titleLink, imageLink, descriptionLink, id } = props.post;
-    const { token } = useContext(UserContext);
-    const [ loading, setLoading ] = useState(false);
-    const [likePost, setLikePost] = useState(false);
-    const [likesCount, setLikesCount] = useState(0);
-    const tokenJwt = !token.token ? JSON.parse(localStorage.getItem("tokenUser")) : token;
+  const {
+    link,
+    description,
+    picture,
+    username,
+    titleLink,
+    imageLink,
+    descriptionLink,
+    id,
+  } = props.post;
+  const { token } = useContext(UserContext);
+  const { loading, setLoading } = useContext(LoadingContext);
+  const [likePost, setLikePost] = useState(false);
+  const tokenJwt = !token.token
+    ? JSON.parse(localStorage.getItem("tokenUser"))
+    : token;
 
-    const URL = "https://projeto17-linkr-cdio.herokuapp.com/"; 
-    //const URL = "http://localhost:4000/";
+  const URL = "https://projeto17-linkr-cdio.herokuapp.com/";
+  // const URL = "http://localhost:4000/";
 
-    useEffect(() => {
-        checkLikePublishing();
-        getLikesCount();
-    }, [loading]);
-    
-    function redirectToLink(){
-        window.open(link, '_blank');
-    }
+  useEffect(() => {
+    checkLikePublishing();
+    // eslint-disable-next-line
+  }, [loading]);
 
-    function likePublishing(){
-        console.log(props.post); setLoading(true);
+  function redirectToLink() {
+    window.open(link, "_blank");
+  }
 
-        const promise = axios.post(`${URL}posts/like`, { idPost:id }, {
-            headers: {
-              Authorization: `Bearer ${tokenJwt.token}`,
-            }
-        });
-        promise.then((response) => {
-            setLikePost(true);
-            console.log(response); setLoading(false);
-        });
-        promise.catch((error) => {
-            console.log(error); setLoading(false);
+  function likePublishing() {
+    console.log(props.post);
+    setLoading(true);
 
-        });
-    };
-
-    function checkLikePublishing(){
-        setLoading(true);
-
-        const promise = axios.post(`${URL}posts/checklike`, { idPost:id }, {
-            headers: {
-              Authorization: `Bearer ${tokenJwt.token}`,
-            }
-        });
-        promise.then((response) => {
-            setLikePost(response.data); 
-            setLoading(false);
-        });
-        promise.catch((error) => {
-            console.log(error); setLoading(false);
-        });
-    };
-
-    function getLikesCount(){
-        setLoading(true);
-
-        const promise = axios.post(`${URL}posts/likecount`, { idPost:id });
-        
-        promise.then((response) => {
-            setLikesCount(response.data); 
-            setLoading(false);
-        });
-        promise.catch((error) => {
-            console.log(error); setLoading(false);
-        });
-    }
-
-    return (
-
-        <Div>
-            <div className="right-container">
-                <img src={picture} alt={username}></img>
-                {likePost?
-                    <IoHeart className="likebutton marked" onClick={likePublishing}/>:
-                    <IoHeartOutline className="likebutton" onClick={likePublishing}/>
-                }
-                <p>{likesCount} likes</p>
-            </div>
-            <div className="left-container">
-                <p className="username">{username}</p>
-                <p className="description">{description}</p>
-
-                <div className="link-metadata" onClick={() => redirectToLink()}>
-                    <div className="container-title-description">
-                        <p className="link-title">{titleLink}</p>
-                        <p className="link-description">{descriptionLink}</p>
-                        <p className="link-url">{link}</p>
-                    </div>
-                    <img src={imageLink} alt="Article"></img>
-                </div>
-            </div>
-        </Div>
+    const promise = axios.post(
+      `${URL}posts/like`,
+      { idPost: id },
+      {
+        headers: {
+          Authorization: `Bearer ${tokenJwt.token}`,
+        },
+      }
     );
+    promise.then((response) => {
+      setLikePost(true);
+      console.log(response);
+      setLoading(false);
+    });
+    promise.catch((error) => {
+      console.log(error);
+      setLoading(false);
+    });
+  }
+
+  function checkLikePublishing() {
+    setLoading(true);
+
+    const promise = axios.post(
+      `${URL}posts/checklike`,
+      { idPost: id },
+      {
+        headers: {
+          Authorization: `Bearer ${tokenJwt.token}`,
+        },
+      }
+    );
+    promise.then((response) => {
+      setLikePost(response.data);
+      setLoading(false);
+    });
+    promise.catch((error) => {
+      console.log(error);
+      setLoading(false);
+    });
+  }
+
+  return (
+    <Div>
+      <div className="right-container">
+        <img src={picture} alt={username}></img>
+        {likePost ? (
+          <IoHeart className="likebutton marked" onClick={likePublishing} />
+        ) : (
+          <IoHeartOutline className="likebutton" onClick={likePublishing} />
+        )}
+      </div>
+      <div className="left-container">
+        <p className="username">{username}</p>
+        <p className="description">{description}</p>
+
+        <div className="link-metadata" onClick={() => redirectToLink()}>
+          <div className="container-title-description">
+            <p className="link-title">{titleLink}</p>
+            <p className="link-description">{descriptionLink}</p>
+            <p className="link-url">{link}</p>
+          </div>
+          <img src={imageLink} alt="Article"></img>
+        </div>
+      </div>
+    </Div>
+  );
 }
 
 const Div = styled.div`
-height: auto;
-width: 100vw;
-max-width: 100vw;
-background-color: #171717;
-margin-top: 19px;
-padding: 9px 18px 15px 15px;
-display: flex;
-justify-content: space-between;
+  height: auto;
+  width: 100vw;
+  max-width: 100vw;
+  background-color: #171717;
+  margin-top: 19px;
+  padding: 9px 18px 15px 15px;
+  display: flex;
+  justify-content: space-between;
 
-.likebutton {
-    width:100%;
+  .likebutton {
+    width: 100%;
     margin-top: 10px;
     align-items: center;
     font-size: 20px;
-    color: #FFFFFF;
-};
+    color: #ffffff;
+  }
 
-.marked {
-    color: #AC0000;
-}
+  .marked {
+    color: #ac0000;
+  }
 
-.right-container img {
+  .right-container img {
     border-radius: 50%;
     width: 44px;
     height: 44px;
     object-fit: cover;
     margin-left: 12px;
-}
+  }
 
-.right-container {
+  .right-container {
     margin-right: 14px;
-}
+  }
 
-.right-container p {
-    font-family: 'Lato', normal;
-    font-weight: 400;
-    font-size: 9px;
-    line-height: 11px;
-    text-align: center;
-    color: #FFFFFF;
-}
-
-.left-container {
+  .left-container {
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     padding-top: 4px;
-}
-.username {
-    font-family: 'Lato';
+  }
+  .username {
+    font-family: "Lato";
     font-size: 17px;
     line-height: 20px;
-    color: #FFFFFF;
-}
+    color: #ffffff;
+  }
 
-.description {
-    font-family: 'Lato';
+  .description {
+    font-family: "Lato";
     font-weight: 400;
     font-size: 15px;
     line-height: 18px;
-    color: #B7B7B7;
+    color: #b7b7b7;
     margin-top: 7px;
     margin-bottom: 13px;
-}
+  }
 
-.link-metadata {
-    max-width: 100%;
+  .link-metadata {
+    width: 100%;
+    height: 115px;
     display: flex;
-    border: 1px solid #4D4D4D;
+    border: 1px solid #4d4d4d;
     border-radius: 11px;
-}
+  }
 
-.link-metadata img {
+  .link-metadata img {
     width: 95px;
     height: auto;
-    object-fit: cover;  
-    border-radius: 0 11px 11px 0;  
-}
+    object-fit: cover;
+    border-radius: 0 11px 11px 0;
+  }
 
-.container-title-description {
+  .container-title-description {
     padding: 7px 7px 8px 11px;
-}
+  }
 
-.link-title {
-    font-family: 'Lato';
+  .link-title {
+    font-family: "Lato";
     font-size: 11px;
     line-height: 13px;
-    color: #CECECE;
+    color: #cecece;
     margin-bottom: 4px;
-}
+    height: 26px;
+    width: 175px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 
-.link-description {
-    font-family: 'Lato';
+  .link-description {
+    font-family: "Lato";
     font-size: 9px;
     line-height: 11px;
-    color: #9B9595;
+    color: #9b9595;
     margin-bottom: 4px;
-}
+    height: 44px;
+    width: 175px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 
-.link-url {
-    font-family: 'Lato';
+  .link-url {
+    font-family: "Lato";
     font-size: 9px;
     line-height: 11px;
-    color: #CECECE;
-}
+    color: #cecece;
+    height: 22px;
+    width: 175px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 `;
